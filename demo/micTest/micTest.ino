6fd5ca6f8d68036f6b6ac5dacad3ca6e340703e8
William Jang <wjang20@amherst.edu>
2019-10-25 18:05:21 -0400

    reduced amp first teensy

--- demo/micTest/micTest.ino
+++ demo/micTest/micTest.ino
@@ -1,12 +1,19 @@
-#define PIN_ANALOG_IN A1
-#define PIN_ANALOG_OUT 9
-#define PIN_ANALOG_MID_OUT A0
+#define PIN_ANALOG_IN 13
+#define PIN_ANALOG_OUT A22
+#define PIN_ANALOG_MID_OUT A21
+#define PIN_PWM_OUT_LED 2
+
 void setup()
 {
   Serial.begin(9600);
   
   // Display status
   Serial.println("Initialized");
+
+  pinMode(PIN_ANALOG_IN, INPUT);
+  pinMode(PIN_ANALOG_OUT, OUTPUT);
+  pinMode(PIN_ANALOG_MID_OUT, OUTPUT);
+  pinMode(PIN_PWM_OUT_LED, OUTPUT);
 
   analogWrite(PIN_ANALOG_MID_OUT, 127);
 }
@@ -21,8 +28,11 @@ void loop()
   value = analogRead(PIN_ANALOG_IN);
   value = 1023 - value;
   samples = 50;
+
+  //value = max(value, 900);
   
-  freq = map(value, 0, 1023, 1, 200);
+  //freq = map(value, 900, 1023, 1, 200);
+  freq = map(value, 0,1023,1,200);
   
   // Convert envelope value into a message
   Serial.print("Status: ");
@@ -32,8 +42,10 @@ void loop()
   // sin wave
   for (in = 0; in < 6.283; in = in + sin_step)
   {
-    out = sin(in) * 127.5 + 127.5;
+    //out = sin(in) * 127.5 + 127.5;
+    out = sin(in)*50 + 127.5;
     analogWrite(PIN_ANALOG_OUT,out);
+    analogWrite(PIN_PWM_OUT_LED, out);
     delay(freq);
   }
 
